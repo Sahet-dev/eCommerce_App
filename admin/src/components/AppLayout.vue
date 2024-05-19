0d6dcbe4ab77b7684342c429a2e7d55ff0cbcd2b
Sahetmyrat <kakajansahy@gmail.com>
2024-05-19 12:30:49 +0300

    Implement responsive Sidebar layout

--- admin/src/components/AppLayout.vue
+++ admin/src/components/AppLayout.vue
@@ -4,7 +4,7 @@
     <div class="min-h-full flex bg-gray-200">
 
 
-        <SidebarLayout :class="{'-ml-[200px]' : sidebarOpen}"/>
+        <SidebarLayout :class="{'-ml-[200px]' : !sidebarOpen}"/>
 
 
 
@@ -27,24 +27,55 @@
     </div>
 </template>
 <script setup>
-import {ref} from "vue";
+import {ref, onMounted, onUnmounted} from "vue";
 import SidebarLayout from "./SidebarLayout.vue";
 import TopHeader from "./TopHeader.vue";
+
 const {title} = defineProps({
     title: String
 })
-
-const sidebarOpen = ref(false);
-
+const sidebarOpen = ref(true);
 const emit = defineEmits(['submit'])
-
 
 function toggleSidebar() {
     sidebarOpen.value = !sidebarOpen.value
 }
+
+onMounted(()=>{
+    handleSidebarOpened()
+    window.addEventListener('resize' , handleSidebarOpened)
+})
+
+function handleSidebarOpened() {
+    sidebarOpen.value = window.outerWidth > 768
+}
+
+onUnmounted(()=>{
+    window.removeEventListener('resize' , handleSidebarOpened)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </script>
-
-
 <style scoped>
 
 </style>
